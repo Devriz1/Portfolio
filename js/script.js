dc38b5ba36ba5b34cc33065e6be5a87f9ad9d344
Mohammed Risal <94342524+Devriz1@users.noreply.github.com>
2025-04-05 10:38:21 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,162 @@
+// htmlcss progress circular bar 
+let htmlProgress = document.querySelector(".html-css"),
+  htmlValue = document.querySelector(".html-progress");
+
+let htmlStartValue = 0,
+  htmlEndValue = 90,
+  htmlspeed = 30;
+
+let progresshtml = setInterval(() => {
+  htmlStartValue++;
+
+  htmlValue.textContent = `${htmlStartValue}%`;
+  htmlProgress.style.background = `conic-gradient(#fca61f ${
+    htmlStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (htmlStartValue == htmlEndValue) {
+    clearInterval(progresshtml);
+  }
+}, htmlspeed);
+
+// javasript progress circular bar 
+let javascriptProgress = document.querySelector(".javascript"),
+  javascriptValue = document.querySelector(".javascript-progress");
+
+let javascriptStartValue = 0,
+  javascriptEndValue = 75,
+  jsspeed = 30;
+
+let progressjs = setInterval(() => {
+  javascriptStartValue++;
+
+  javascriptValue.textContent = `${javascriptStartValue}%`;
+  javascriptProgress.style.background = `conic-gradient(#7d2ae8 ${
+    javascriptStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (javascriptStartValue == javascriptEndValue) {
+    clearInterval(progressjs);
+  }
+}, jsspeed);
+
+// php progress circular bar 
+let phpProgress = document.querySelector(".python"),
+  phpValue = document.querySelector(".python-progress");
+
+let phpStartValue = 0,
+  phpEndValue = 80,
+  phpspeed = 30;
+
+let progressphp = setInterval(() => {
+  phpStartValue++;
+
+  phpValue.textContent = `${phpStartValue}%`;
+  phpProgress.style.background = `conic-gradient(#20c997 ${
+    phpStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (phpStartValue == phpEndValue) {
+    clearInterval(progressphp);
+  }
+}, phpspeed);
+
+// reactjs progress circular bar 
+let reactProgress = document.querySelector(".php"),
+  reactValue = document.querySelector(".php-progress");
+
+let reactStartValue = 0,
+  reactEndValue = 50,
+  rjsspeed = 30;
+
+let progressreact = setInterval(() => {
+  reactStartValue++;
+
+  reactValue.textContent = `${reactStartValue}%`;
+  reactProgress.style.background = `conic-gradient(#3f396d ${
+    reactStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (reactStartValue == reactEndValue) {
+    clearInterval(progressreact);
+  }
+}, rjsspeed);
+
+
+// filter using javascript
+$(document).ready(function () {
+  $(".filter-item").click(function () {
+    const value = $(this).attr("data-filter");
+    if (value == "all") {
+      $(".post").show("1000");
+    } else {
+      $(".post")
+        .not("." + value)
+        .hide("1000");
+      $(".post")
+        .filter("." + value)
+        .show("1000");
+    }
+  });
+});
+
+
+// javascript for sticky navbar even if u scroll the navbar will be fixed
+document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 50) {
+        document.getElementById('navbar-top').classList.add('fixed-top');
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector('.navbar').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+        document.getElementById('navbar-top').classList.remove('fixed-top');
+         // remove padding top from body
+        document.body.style.paddingTop = '0';
+      } 
+  });
+}); 
+
+
+// adding funtionality to back to top button 
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click",function(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  var formData = new FormData();
+  formData.append("name", document.getElementById("name").value);
+  formData.append("email", document.getElementById("email").value);
+  formData.append("mobile", document.getElementById("mobile").value);
+  formData.append("message", document.getElementById("message").value);
+
+  fetch("https://script.google.com/macros/s/AKfycbyop2iZ4-EwH8N5RnRdp5IqLV1BoLf2-dTTuipOT8cwXPLlBPhCdWpnj1yHRJ9je4AL/exec", { // Replace with your Google Apps Script Web App URL
+    method: "POST",
+    body: formData,
+  }).then(response => {
+    document.getElementById("formStatus").style.display = "block";
+    document.getElementById("contactForm").reset();
+  }).catch(error => console.error("Error!", error));
+});
